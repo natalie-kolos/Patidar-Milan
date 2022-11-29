615112ef669e8449db3b264dda2e895fd4cb3104
natalie-kolos <kolos.natasha@gmail.com>
2022-11-29 09:27:13 +0100

    Profile active (without form)

--- js/common.js
+++ js/common.js
@@ -1,3 +1,34 @@
+$(document).ready(function () {
+  $(".carousel-responsive").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
 (function () {
   "use strict";
 
