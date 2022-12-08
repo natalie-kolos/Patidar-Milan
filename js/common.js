92d84505fbf17f3fc6b8c6fb42c2c5081ad1f886
natalie-kolos <kolos.natasha@gmail.com>
2022-12-08 19:54:49 +0100

    fansy box on the account active page

--- js/common.js
+++ js/common.js
@@ -70,3 +70,75 @@ $(document).ready(function () {
     );
   });
 })();
+
+$( document ).ready(function() {
+  var usaSelect = document.getElementsByClassName("USA");
+  var canadaSelect = document.getElementsByClassName("Canada");
+  if($('#country').val() == 'USA'){
+    for(var i = 0; i < usaSelect.length; i++){
+        usaSelect[i].style.display = "block";
+    }
+  }else if ($('#country').val() == 'Canada'){
+    for(var i = 0; i < canadaSelect.length; i++){
+        canadaSelect[i].style.display = "block";
+    }
+  }
+});
+function yesnoCheck(that) {
+    if (that.value == "yes") {
+        document.getElementById("ifYes").style.display = "block";
+    } else {
+        document.getElementById("ifYes").style.display = "none";
+    }
+}
+function countryCheck(that) {
+  var usaSelect = document.getElementsByClassName("USA");
+  var canadaSelect = document.getElementsByClassName("Canada");
+    if (that.value == "USA") {
+      for(var i = 0; i < usaSelect.length; i++){
+        usaSelect[i].style.display = "block";
+    }
+    for(var i = 0; i < canadaSelect.length; i++){
+      canadaSelect[i].style.display = "none";
+
+    }
+    } else {
+      for(var i = 0; i < canadaSelect.length; i++){
+        canadaSelect[i].style.display = "block";
+    }
+    for(var i = 0; i < usaSelect.length; i++){
+        usaSelect[i].style.display = "none"; 
+    }
+    }
+}
+var input = document.querySelectorAll('.js-date')[0];
+  
+var dateInputMask = function dateInputMask(elm) {
+  elm.addEventListener('keypress', function(e) {
+    if(e.keyCode < 47 || e.keyCode > 57) {
+      e.preventDefault();
+    }
+    
+    var len = elm.value.length;
+    
+    // If we're at a particular place, let the user type the slash
+    // i.e., 12/12/1212
+    if(len !== 1 || len !== 3) {
+      if(e.keyCode == 47) {
+        e.preventDefault();
+      }
+    }
+    
+    // If they don't add the slash, do it for them...
+    if(len === 2) {
+      elm.value += '/';
+    }
+
+    // If they don't add the slash, do it for them...
+    if(len === 5) {
+      elm.value += '/';
+    }
+  });
+};
+  
+dateInputMask(input);
